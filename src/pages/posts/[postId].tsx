19cb31b24bc4e78dd2f0a9851402ffbd0fd78fa3
Nguyet Chau <chau.thi.nguyet@styl.solutions>
2023-06-28 15:17:16 +0700

    file system routing

---
+++ src/pages/posts/[postId].tsx
@@ -0,0 +1,14 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+
+export interface PostDetailProps {}
+
+export default function PostDetail(props: PostDetailProps) {
+  const router = useRouter();
+  return (
+    <div>
+      <h1>Post Detail page</h1>
+      <p>Query: {JSON.stringify(router.query)} </p>
+    </div>
+  );
+}
