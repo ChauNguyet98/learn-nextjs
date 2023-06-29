d0b404fb258434eb9628e02484856b78a0125f54
Nguyet Chau <chau.thi.nguyet@styl.solutions>
2023-06-29 11:54:16 +0700

    automatic static optimization

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,16 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
 export interface AboutProps {}
 
 export default function About(props: AboutProps) {
+  const router = useRouter();
+  console.log('check: ', router.query);
+
   return <div>About page</div>;
+}
+
+export function getServerSideProps() {
+  // Pass data to the page via props
+  return { props: {} };
 }
